953b9d267f09e9deffff4713d08b01a6c7eff395
maocq <carmonaesc@gmail.com>
2023-01-03 15:47:37 -0500

    t

--- sh/start_docker_image.sh
+++ sh/start_docker_image.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 project=$1
 docker stop "$(docker ps -q)"
-cd backend-benchmark/$project
-docker build -t "$project" .
 docker rm -f "$project"
+
+cd app/$project
+docker build -t "$project" .
 
 touch /tmp/env.list
 docker run --env-file /tmp/env.list --name "$project" -d -p 8080:8080 -p 5432:5432 "$project"
