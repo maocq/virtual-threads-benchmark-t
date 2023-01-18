63dd77a61deb269a04253b9ba1167c02a15d29f6
maocq <carmonaesc@gmail.com>
2023-01-18 17:58:03 -0500

    t

--- all.sh
+++ all.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-cases=("go-ms")
+cases=("elixir-ms" "go-ms" "java-helidon-nima-ms" "java-spring2-ms" "java-spring3-native-ms" "java-spring3-no-native-ms")
 
 for case in "${cases[@]}"; do
   echo "Starting $case"
+  #rm sh/.tmp/*
   ./start.sh "$case"
   ./performance-analyzer.sh "$case"
   ./stop.sh "$case"
