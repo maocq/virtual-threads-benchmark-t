120c9fbe477ddfb216ab0831389d7e38961b5434
maocq <carmonaesc@gmail.com>
2023-01-18 21:11:16 -0500

    t

--- sh/install_docker.sh
+++ sh/install_docker.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -e
 
-apt-get update
-apt-get install -y ca-certificates curl gnupg lsb-release
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
 
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
-groupadd docker
-usermod -aG docker ubuntu
+sudo groupadd docker
+sudo usermod -aG docker ubuntu
 newgrp docker
