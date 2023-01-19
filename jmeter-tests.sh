55d188366cdf8d48fd972cfb762ed493b251cb21
maocq <carmonaesc@gmail.com>
2023-01-19 08:48:48 -0500

    t

---
+++ jmeter-tests.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+source ./sh/functions.sh
+
+case=$1
+#case="java-native-ms"
+
+StackName=$(jq -r ".StackName" "config.json")-$case
+User=$(jq -r ".User" "config.json")
+Key=$(jq -r ".Key" "config.json")
+
+outputs=$(aws cloudformation describe-stacks --stack-name $StackName | jq '{Outputs: .Stacks[].Outputs}')
+app_ip=$(echo $outputs | jq -r '.Outputs[] | select(.OutputKey == "PublicIPApp") | .OutputValue')
+app_private_ip=$(echo $outputs | jq -r '.Outputs[] | select(.OutputKey == "PrivateIPApp") | .OutputValue')
+tests_ip=$(echo $outputs | jq -r '.Outputs[] | select(.OutputKey == "PublicIPTest") | .OutputValue')
+
+for FILE in test/jmeter/*; do
+    name_file=$(basename $FILE);
+    scenario=$(basename -s .exs $FILE);
+
+    command="docker restart \$(docker ps -a -q)"
+    execute_remote_command "$command" "$app_ip" "$User" "$Key" > /dev/tty
+
+    wait_http "http://$app_ip:8080/api/hello"
+
+    cp $FILE "sh/.tmp/jmeter-$name_file"
+    sed -i -e "s/_SERVICE_IP_/$app_private_ip/g" "sh/.tmp/jmeter-$name_file"
+    upload_file $tests_ip "sh/.tmp/jmeter-$name_file" "JMeterBenchmark.jmx" $User $Key
+
+    _out=$(execute_remote_command "sudo rm -rf report && sudo rm -f result-jmeter.csv && mkdir -p report" "$tests_ip" "$User" "$Key")
+    execute_remote_command "docker run --rm -i -v \${PWD}:\${PWD} -w \${PWD} justb4/jmeter:latest -n -t JMeterBenchmark.jmx -l result-jmeter.csv -e -o report" "$tests_ip" "$User" "$Key" > /dev/tty
+    _out=$(execute_remote_command "tar -zcvf report.tar.gz report" "$tests_ip" "$User" "$Key")
+
+    download_file $tests_ip "report.tar.gz" "sh/.tmp/j-$scenario-$case.tar.gz" $User $Key
+    echo "-------> $case $scenario" > /dev/tty
+done
+
+echo "#################"
+echo "$case"
+echo "#################"
